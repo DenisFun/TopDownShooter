b9aec94db8f51e93937fe546ba8270204bac74f0
Denis <d.afanasyev@xlab-game.com>
2023-01-11 17:57:20 +0300

    Second commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class PlayerController : MonoBehaviour
+    {
+    //    public void OnMove(CallbackContext Context)
+    //    {
+        
+    //    }
+    }
+}
